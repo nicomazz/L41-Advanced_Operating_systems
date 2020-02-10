fe717f87a3949a5a6f74206bc6cc2b4c9ed0071e
nicomazz <nicolmazzucato@me.com>
2020-02-10 19:50:39 +0000

    Report 1 finished

---
+++ lab/1/data/lab1/scripts/flow.d
@@ -0,0 +1,37 @@
+#pragma D option flowindent
+
+BEGIN {
+}
+
+syscall::clock_gettime:return
+/execname == "io-static" && !self->in_benchmark/
+{
+    self->in_benchmark = 1;
+    self->cstart = vtimestamp;
+}
+
+syscall::clock_gettime:entry
+/execname == "io-static" && self->in_benchmark/
+{
+    self->in_benchmark = 0;
+    trace(vtimestamp - self->cstart);
+}
+
+syscall::execve:return
+/execname == "io-static" && !self->in_benchmark/
+{
+    /* self->in_benchmark = 1; */
+    self->star_time = vtimestamp;
+}
+
+syscall::exit:entry
+/execname == "io-static" && self->in_benchmark/
+{
+    self->in_benchmark = 0;
+    printf("tot %d",timestamp - self->star_time); 
+}
+
+
+fbt:::
+/self->in_benchmark && self->i++ < 10000/ 
+{}
