d585f38abec5bb0501f6afa7a64569d7c67c8208
nicomazz <nicomazz97@gmail.com>
2020-03-22 00:18:03 +0000

    Starting working on last assignment

--- lab/4-5/lib/tcp.py
+++ lab/4-5/lib/tcp.py
@@ -1,9 +1,7 @@
 import json
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-import re
-from collections import defaultdict
+
+import pygraphviz as pgv
+from IPython.display import Image
 from dtrace import *
 
 
@@ -29,3 +27,87 @@ def setup_pipes(latency=10):
     cmd("ifconfig lo0 mtu 1500")
     cmd("ipfw add 1 pipe 1 tcp from any 10141 to any via lo0")
     cmd("ipfw add 2 pipe 2 tcp from any to any 10141 via lo0")
+
+"""
+ fbt::tcp_do_segment:entry {
+        trace((unsigned int)args[1]->th_seq);
+        trace((unsigned int)args[1]->th_ack);
+        trace(tcp_state_string[args[3]->t_state]);
+    }
+"""
+def graph_tcp(latency):
+    set_latency(latency)
+
+
+    tcp_state_change_script = """
+   
+    
+    fbt::tcp_state_change:entry {
+        printf("{\\"timestamp\\": %u, \\"local_port\\": %u, \\"foreign_port\\": %u, \\"previous_tcp_state\\": \\"%s\\", \\"tcp_state\\": \\"%s\\"}", 
+        walltimestamp,
+        ntohs(args[0]->t_inpcb->inp_inc.inc_ie.ie_lport),
+        ntohs(args[0]->t_inpcb->inp_inc.inc_ie.ie_fport),
+        tcp_state_string[args[0]->t_state],
+        tcp_state_string[args[1]]);
+
+        stack();
+    }
+    """
+
+    # Callback invoked to process the aggregation
+    values = []
+
+    def simple_out(raw_value):
+        values.append(raw_value)
+
+    # Create a seperate thread to run the DTrace instrumentation
+    dtrace_thread = DTraceConsumerThread(tcp_state_change_script,
+                                         out_func=simple_out,
+                                         chew_func=lambda v: None,
+                                         chewrec_func=lambda v: None,
+                                         walk_func=None,
+                                         sleep=1)
+
+    # Start the DTrace instrumentation
+    dtrace_thread.start()
+
+    # Display header to indicate that the benchmarking has started
+    print("Running ipc benchmark")
+
+    # Run the ipc-static benchmark
+    benchmark_output = cmd("ipc/ipc-static -v -i tcp 2thread")
+
+    # The benchmark has completed - stop the DTrace instrumentation
+    dtrace_thread.stop()
+    dtrace_thread.join()
+    dtrace_thread.consumer.__del__()
+
+    label = "TCP state machine - {} ms latency".format(latency)
+    output_file = "TCP_state_machine_{}_ms.png".format(latency)
+    tcp_state_machine = pgv.AGraph(
+        label=label, strict=False, directed=True)
+    for raw_value in values:
+        try:
+            value = json.loads(raw_value)
+            #print(value)
+            # JSON formatted string
+            if value['previous_tcp_state'] is not None and value['tcp_state'] is not None:
+                from_state = value['previous_tcp_state'][6:]
+                to_state = value['tcp_state'][6:]
+                label = "server" if value["local_port"] == 10141 else "client"
+
+                # print "State transition {} -> {}".format(
+                #    value['previous_tcp_state'], value['tcp_state'])
+            else:
+                print "String malformatted missing previous_tcp_state of tcp_state fields"
+        except ValueError as e:  # stack trace
+            prec_f = "\n".join([i.replace('`', '+').split("+")[1] for i in raw_value.split('\n')[1:]][::-1])
+            tcp_state_machine.add_edge(from_state, to_state,
+                                       label=label + "\n({})".format(prec_f), color='green')
+
+            # Raw string - manually post-process
+            # print "Preceeding stack frame {}".format(raw_value.split('\n')[1])
+
+    print("Completed")
+    tcp_state_machine.draw(path=output_file, format='png', prog='dot')
+    return Image(output_file)
--- lab/4-5/readme.md
+++ lab/4-5/readme.md
@@ -1,6 +1,11 @@
+# TCP lab
+
 - `tcpcb`: tcp control block. defined here:
-   https://sourcegraph.com/github.com/freebsd/freebsd@df2262014eee4a534988dbeae57cdc02e014020c/-/blob/sys/netinet/tcp_var.h#L94
+   <https://sourcegraph.com/github.com/freebsd/freebsd@df2262014eee4a534988dbeae57cdc02e014020c/-/blob/sys/netinet/tcp_var.h#L94>
 - states definitions: https://sourcegraph.com/github.com/freebsd/freebsd@67441e15d85bfacb98815d0b7e575afed0bbba1c/-/blob/sys/netinet/tcp_fsm.h#L47:1
+- MSL: Maximum segment lifetime
+
+
 ```c
 #define	TCP_NSTATES	11
 
@@ -19,3 +24,56 @@
 #define	TCPS_FIN_WAIT_2		9	/* have closed, fin is acked */
 #define	TCPS_TIME_WAIT		10	/* in 2*msl quiet wait after close */
 ```
+
+## TCP header
+```c
+/*
+ * TCP header.
+ * Per RFC 793, September, 1981.
+ */
+struct tcphdr {
+	u_short	th_sport;		/* source port */
+	u_short	th_dport;		/* destination port */
+	tcp_seq	th_seq;			/* sequence number */
+	tcp_seq	th_ack;			/* acknowledgement number */
+#if BYTE_ORDER == LITTLE_ENDIAN
+	u_char	th_x2:4,		/* (unused) */
+		th_off:4;		/* data offset */
+#endif
+#if BYTE_ORDER == BIG_ENDIAN
+	u_char	th_off:4,		/* data offset */
+		th_x2:4;		/* (unused) */
+#endif
+	u_char	th_flags;
+#define	TH_FIN	0x01
+#define	TH_SYN	0x02
+#define	TH_RST	0x04
+#define	TH_PUSH	0x08
+#define	TH_ACK	0x10
+#define	TH_URG	0x20
+#define	TH_ECE	0x40
+#define	TH_CWR	0x80
+#define	TH_AE	0x100			/* maps into th_x2 */
+#define	TH_FLAGS	(TH_FIN|TH_SYN|TH_RST|TH_PUSH|TH_ACK|TH_URG|TH_ECE|TH_CWR)
+#define	PRINT_TH_FLAGS	"\20\1FIN\2SYN\3RST\4PUSH\5ACK\6URG\7ECE\10CWR\11AE"
+
+	u_short	th_win;			/* window */
+	u_short	th_sum;			/* checksum */
+	u_short	th_urp;			/* urgent pointer */
+};
+```
+
+### struct tcbcb
+
+IT is pretty long: <https://sourcegraph.com/github.com/freebsd/freebsd@7f6b5f56c043835267db0f98eb2ec083c5a0bbee/-/blob/sys/netinet/tcp_var.h#L94:1>
+
+
+```
+dtrace -n 'fbt::tcp_do_segment:entry {trace((unsigned int)args[1]->th_seq);trace((unsigned int)args[1]->th_ack);trace(tcp_state_string[args[3]->t_state]);}'
+
+```
+
+### Notes
+
+- the tcp RFC came out in 1981, then the paper congestion avoidance and control in 1988. This was interesting
+- 
